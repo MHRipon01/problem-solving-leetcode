6f6754164e310dae74322f6dba797fc575a9947d
MHRipon01 <mhripon012@gmail.com>
2025-02-20 12:00:00 +0600

    leetcode problem

---
+++ p43_leetcode463.cpp
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/island-perimeter/
+class Solution {
+    public:
+    bool vis[105][105]
+        int islandPerimeter(vector<vector<int>>& grid) {
+         int n = grid.size();
+         int m = grid[0].size();
+    memset(vis,false , sizeof(vis));
+    
+        }
+    };
