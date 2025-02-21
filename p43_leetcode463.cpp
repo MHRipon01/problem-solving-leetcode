69c15718223ab5e42f8ed22253c8c5caf8667c86
MHRipon01 <mhripon012@gmail.com>
2025-02-21 23:17:24 +0600

    leetcode

--- p43_leetcode463.cpp
+++ p43_leetcode463.cpp
@@ -1,11 +1,33 @@
-//https://leetcode.com/problems/island-perimeter/
-class Solution {
-    public:
-    bool vis[105][105]
-        int islandPerimeter(vector<vector<int>>& grid) {
-         int n = grid.size();
-         int m = grid[0].size();
-    memset(vis,false , sizeof(vis));
-    
+// https://leetcode.com/problems/island-perimeter/
+class Solution
+{
+public:
+    bool vis[105][105];
+    vector<pair<int, int>> d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    void dfs(int si, sj)
+    {
+        vis[si][sj] = true;
+        for (int i = 0; i < 4; i++)
+        {
+            int ci = si + d[i].first;
+            int cj = sj + d[i].second;
         }
-    };
+    }
+    int islandPerimeter(vector<vector<int>> &grid);
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        memset(vis, false, sizeof(vis));
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (!vis[i][j] && grid[i][j] == 1)
+                {
+                    dfs(i, j);
+                }
+            }
+        }
+    }
+};
