5cdefb3e82c49496854c64ca3d73b89a8e841277
MHRipon01 <mhripon012@gmail.com>
2025-03-14 17:29:45 +0600

    p54 attempted

--- p54atcoder.cpp
+++ p54atcoder.cpp
@@ -1,1 +1,15 @@
-//https://atcoder.jp/contests/abc220/tasks/abc220_a?lang=en
+// https://atcoder.jp/contests/abc220/tasks/abc220_a?lang=en
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a > (c * 2) || (c * 2) > b)
+        cout << -1 ;
+    else
+        cout << c * 2 << endl;
+    return 0;
+}
