60cae17e7955ba36aefadba0f660cb19268d4db4
MHRipon01 <mhripon012@gmail.com>
2025-03-17 10:40:05 +0600

    p-55 soved

--- p55_atcoder.cpp
+++ p55_atcoder.cpp
@@ -1,2 +1,24 @@
 // https://atcoder.jp/contests/abc124/tasks/abc124_a?lang=en
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, doubleA, doubleB, sumOfAB;
+
+    cin >> a >> b;
+    doubleA = a + (a - 1);
+    doubleB = b + (b - 1);
+    sumOfAB = a + b;
+
+    if (doubleA > doubleB && doubleA > sumOfAB)
+    {
+        cout << doubleA << endl;
+    }
+    else if (doubleA < doubleB && doubleB > sumOfAB)
+        cout << doubleB << endl;
+    else
+        cout << sumOfAB << endl;
+    return 0;
+}
