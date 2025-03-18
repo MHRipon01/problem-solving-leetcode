b1030d90f28f5ac2a370a6a503266bbb97b121d7
MHRipon01 <mhripon012@gmail.com>
2025-03-18 10:40:05 +0600

    p-55 another approach

--- p55_atcoder.cpp
+++ p55_atcoder.cpp
@@ -22,3 +22,21 @@ int main()
         cout << sumOfAB << endl;
     return 0;
 }
+
+// other approach
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int case1 = a + (a - 1); 
+    int case2 = b + (b - 1);  
+    int case3 = a + b;        
+
+    cout << max({case1, case2, case3}) << endl;
+
+    return 0;
+}
