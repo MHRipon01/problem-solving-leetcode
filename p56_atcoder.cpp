e0ee1b6f6b3d9bc0e818851c21cef0e6285ca08b
MHRipon01 <mhripon012@gmail.com>
2025-03-20 09:55:30 +0600

    p56 another approach

--- p56_atcoder.cpp
+++ p56_atcoder.cpp
@@ -1,5 +1,26 @@
 // https://atcoder.jp/contests/abc222/tasks/abc222_a?lang=en
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     string s = to_string(n);
+//     int length = 4 - s.length();
+//     for (int i = 1; i <= length; i++)
+//     {
+//         s = '0' + s;
+//     }
+
+//     cout << s << endl;
+
+//     return 0;
+// }
+
+//  ANOTHER APPROACH
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -9,13 +30,11 @@ int main()
     cin >> n;
 
     string s = to_string(n);
-    int length = 4 - s.length();
-    for (int i = 1; i <= length; i++)
+
+    while (s.length() < 4)
     {
         s = '0' + s;
     }
-
     cout << s << endl;
-
     return 0;
 }
