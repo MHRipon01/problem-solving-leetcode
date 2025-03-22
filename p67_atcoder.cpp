425641500d64a30a13d57607e00533d22567f6e8
MHRipon01 <mhripon012@gmail.com>
2025-03-22 11:12:29 +0600

    p67 another approach

--- p67_atcoder.cpp
+++ p67_atcoder.cpp
@@ -1,18 +1,35 @@
-// https://atcoder.jp/contests/abc125/tasks/abc125_a?lang=en
+// // https://atcoder.jp/contests/abc125/tasks/abc125_a?lang=en
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int a, b, t, cnt = 0;
+//     cin >> a >> b >> t;
+//     int time = a;
+//     while (time <= t)
+//     {
+//         cnt++;
+//         time += a;
+//     }
+//     cout << cnt * b << endl;
+
+//     return 0;
+// }
+
+// ANOTHER APPROACH
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int a, b, t, cnt = 0;
-    cin >> a >> b >> t;
-    int time = a;
-    while (time <= t)
-    {
-        cnt++;
-        time += a;
-    }
-    cout << cnt * b << endl;
+int main() {
+    int A, B, T, count = 0;
+    cin >> A >> B >> T;
 
+    for (int time = A; time <= T; time += A) {
+        count++;  
+    }
+
+    cout << count * B << endl;  
     return 0;
 }
