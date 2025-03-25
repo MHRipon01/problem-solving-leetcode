2ce2467204dbb266fdada246f0c5160089186c90
MHRipon01 <mhripon012@gmail.com>
2025-03-25 15:12:43 +0600

    p59 logic

--- p59_atcoder.cpp
+++ p59_atcoder.cpp
@@ -1,2 +1,18 @@
-//https://atcoder.jp/contests/abc214/tasks/abc214_b?lang=env
+// https://atcoder.jp/contests/abc214/tasks/abc214_b?lang=env
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int a, b, c, s, t, count = 0;
+
+    for(a=0; a<=s ; a++){
+        for(b=0; b<=s-a; b++){
+            
+        }
+    }
+
+    return 0;
+}
