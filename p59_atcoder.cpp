8b6fe9003c1565a5b571f90d13af0ff5e100139b
MHRipon01 <mhripon012@gmail.com>
2025-03-26 11:05:57 +0600

    p59 solved

--- p59_atcoder.cpp
+++ p59_atcoder.cpp
@@ -7,12 +7,21 @@ int main()
 {
 
     int a, b, c, s, t, count = 0;
-
-    for(a=0; a<=s ; a++){
-        for(b=0; b<=s-a; b++){
-            
+    cin >> s >> t;
+    for (a = 0; a <= s; a++)
+    {
+        for (b = 0; b <= s - a; b++)
+        {
+            for (c = 0; c <= s - a - b; c++)
+            {
+                if (a * b * c <= t)
+                {
+                    count++;
+                }
+            }
         }
     }
 
+    cout << count<< endl;
     return 0;
 }
