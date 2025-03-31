1e32c469fda3f0c2791f5411c914fb25154c2163
MHRipon01 <mhripon012@gmail.com>
2025-03-31 09:22:14 +0600

    p64

---
+++ p64_codeforces.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+    long long int sum = 0;
+    int min_odd = INT_MAX;
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        sum += arr[i];
+
+        if(arr[i] % 2 == 1)
+        {
+            min_odd = min(min_odd, arr[i]);
+        }
+    }
+
+    if(sum % 2 == 1)
+    {
+        sum -= min_odd;
+    }
+
+    cout<<sum<<endl;
+    return 0;
+}
