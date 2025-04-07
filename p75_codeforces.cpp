801d63fa5203f58acb6f0d2ceff07a4c0c34d735
MHRipon01 <mhripon012@gmail.com>
2025-04-07 14:46:34 +0600

    p75 done

--- p75_codeforces.cpp
+++ p75_codeforces.cpp
@@ -1,74 +1,42 @@
+// https://codeforces.com/problemset/problem/2092/B
 #include <bits/stdc++.h>
 using namespace std;
 
-struct UnionFind {
-    vector<int> parent;
-    UnionFind(int n) : parent(n) {
-        iota(parent.begin(), parent.end(), 0);
-    }
-    int find(int u) {
-        if (parent[u] != u)
-            parent[u] = find(parent[u]);
-        return parent[u];
-    }
-    void unite(int u, int v) {
-        u = find(u), v = find(v);
-        if (u != v)
-            parent[v] = u;
-    }
-};
-
-void solve() {
-    int n;
+string sol()
+{
+    int n, countOdd = 0, countEven = 0;
+    cin >> n;
     string a, b;
-    cin >> n >> a >> b;
-    
-    int sum_a = 0;
-    for (char c : a) sum_a += (c == '1');
-    if (sum_a % 2 != 0) {
-        cout << "NO\n";
-        return;
-    }
-    
-    int total_nodes = 2 * n;
-    UnionFind uf(total_nodes);
-    
-    for (int i = 1; i < n; ++i) { // i is 0-based, representing original i >=2
-        // a[i] and b[i-1]
-        int a_node = i;
-        int b_prev_node = n + (i - 1);
-        uf.unite(a_node, b_prev_node);
-        // b[i] and a[i-1]
-        int b_node = n + i;
-        int a_prev_node = i - 1;
-        uf.unite(b_node, a_prev_node);
-    }
-    
-    vector<int> a_ones(total_nodes, 0), b_zeros(total_nodes, 0);
-    for (int i = 0; i < n; ++i) {
-        int root = uf.find(i);
-        a_ones[root] += (a[i] == '1');
-    }
-    for (int i = 0; i < n; ++i) {
-        int root = uf.find(n + i);
-        b_zeros[root] += (b[i] == '0');
-    }
-    
-    for (int i = 0; i < n; ++i) {
-        int root = uf.find(i);
-        if (a_ones[root] > 0 && b_zeros[root] == 0) {
-            cout << "NO\n";
-            return;
+    cin >> a >> b;
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            countEven += a[i] == '0';
+            countOdd += b[i] == '0';
+        }
+        else
+        {
+            countOdd += a[i] == '0';
+            countEven += b[i] == '0';
         }
     }
-    cout << "YES\n";
+
+    return ((countEven >= (n + 1) / 2 && countOdd >= n / 2) ? "YES" : "NO");
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
+int main()
+{
+    ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
     int t;
     cin >> t;
-    while (t--) solve();
+
+    while (t--)
+    {
+       cout << sol() << endl;
+    }
+
     return 0;
 }
