07406790b8af5fee43f5f5f9427a4d85456d8174
MHRipon01 <mhripon012@gmail.com>
2025-04-08 17:48:14 +0600

    p76

---
+++ A_Make_it_White.cpp
@@ -0,0 +1,18 @@
+//https://codeforces.com/problemset/problem/1927/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+
+    }
+
+    return 0;
+}
