95fd2e776ca07ed3ba721ea75db6456ce60aefd0
MHRipon01 <mhripon012@gmail.com>
2025-05-02 14:32:05 +0600

    p145 done

---
+++ p145_Football_Ties.cpp
@@ -0,0 +1,25 @@
+//https://www.codechef.com/problems/FOOTBALLTIES?tab=submissions
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        if(x > y ) cout << y%3 << endl;
+        else cout << x%3 << endl;
+
+    }
+
+    return 0;
+}
